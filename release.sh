e3b88ece2d321684f78b1fc5adbbfa198867ac0b
tommander <tomas.rajnoha@proton.me>
2025-05-29 02:01:46 +0200

    Initial gh-pages commit

--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cat fupd_begin src/FactorioUpdater.php fupd_end > fupd.php
-sed -i 's/class FactorioUpdater/class FactorioUpdaterScript/g' fupd.php
--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Bootstrap for unit tests.
- */
-
-declare(strict_types=1);
-
-require __DIR__ . '/../vendor/autoload.php';
